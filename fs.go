f8c65c02f6afec09840ab8e8945db8e91bcca9eb
kumakichi <xyesan@gmail.com>
2018-04-22 18:27:57 +0800

    get local ips

--- fs.go
+++ fs.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"flag"
-	"net/http"
-	"log"
 	"fmt"
 	"github.com/skip2/go-qrcode"
 	"github.com/skratchdot/open-golang/open"
+	"log"
+	"net/http"
 )
 
 var (
-	port int
-	help bool
+	port      int
+	help      bool
 	directory string
 )
 
 func init() {
 	flag.IntVar(&port, "p", 8000, "Listen port")
-	flag.BoolVar(&help, "h",  false, "Print this help infomation")
+	flag.BoolVar(&help, "h", false, "Print this help infomation")
 	flag.StringVar(&directory, "d", ".", "File server root path")
 }
 
@@ -27,19 +27,22 @@ func main() {
 		flag.PrintDefaults()
 		return
 	}
+	ips := getIPs()
+	fmt.Println(ips)
+	return
 
 	http.HandleFunc("/qrcode", func(w http.ResponseWriter, r *http.Request) {
-		b, err :=qrcode.Encode("http://192.168.1.5:8000/file",  qrcode.Highest, 256)
-		if err !=nil {
+		b, err := qrcode.Encode("http://192.168.1.5:8000/file", qrcode.Highest, 256)
+		if err != nil {
 			log.Fatal(err)
 		} else {
 			w.Write(b)
 		}
 	})
-	http.Handle("/file", http.FileServer(http.Dir(directory)))
+	http.Handle("/file/", http.StripPrefix("/file/", http.FileServer(http.Dir(directory))))
 
-	host:=fmt.Sprintf("localhost:%d", port)
+	host := fmt.Sprintf(":%d", port)
 	log.Printf("Listen at %s\n", host)
-	open.Run("http://"+host+"/qrcode")
+	open.Run("http://localhost" + host + "/qrcode")
 	log.Fatal(http.ListenAndServe(host, nil))
 }
---
+++ ip.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+var ips map[string]string
+
+func getIPs() map[string]string {
+	ips = make(map[string]string)
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			log.Printf("localAddresses: %+v\n", err.Error())
+			continue
+		}
+
+		for _, a := range addrs {
+			if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					ips[i.Name] = ipnet.IP.String()
+				}
+			}
+		}
+	}
+
+	return ips
+}
